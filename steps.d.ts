9b4da98fd7c360cd90c16f927410298e6265706c
TsaqifDeniar <158185332+tsaqifdb@users.noreply.github.com>
2024-06-19 01:03:06 +0700

    Menyelesaikan Submision Modul Nomor 3

---
+++ steps.d.ts
@@ -0,0 +1,10 @@
+type steps_file = typeof import('./steps_file.js');
+
+declare namespace CodeceptJS {
+  interface SupportObject { I: I, current: any }
+  interface Methods extends Puppeteer {}
+  interface I extends ReturnType<steps_file> {}
+  namespace Translation {
+    interface Actions {}
+  }
+}
